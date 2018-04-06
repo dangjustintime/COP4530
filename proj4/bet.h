ce5077171a8039ab00f2765d67071d244184e73c
dangjustintime <dangjustintime@gmail.com>
2018-04-06 12:51:56 -0400

    made insertion, working on deletion

--- proj4/bet.h
+++ proj4/bet.h
@@ -1,38 +1,92 @@
 #ifndef PROJ$_BET_H
 #define PROJ4_BET_H
 
+#include <iostream>
 #include <string>
 
 template <typename T>
 class BET {
-  struct BinaryNode {
-    T data;
-    BinaryNode * left;
-    BinaryNode * right;
-  };
-
   public:
+    //constructors
     BET();
     BET(const std::string postfix);
     BET(const BET &);
     ~BET();
-    bool buildFromPostfix(const std::string postfix);
     const BET & operator=(const BET &);
-    void printInfixExpression();
-    void printPostfixExpression();
+    bool buildFromPostfix(const std::string postfix);
+    
+    // insertion and deletion
+    void makeEmpty();
+    void insert(const T & data);
+    void insert(T && data);
+    void remove(const T & data);
+
+    // getters
     size_t size();
     size_t leaf_nodes();
     bool empty();
+    // print functions
+    void printInfixExpression();
+    void printPostfixExpression();
 
   private:
+    // BinaryNode
+    struct BinaryNode {
+      // BinaryNode member data
+      T data;
+      BinaryNode * left ;
+      BinaryNode * right;
+      
+      // BinaryNode constructors
+      BinaryNode(const T & data,
+          BinaryNode * left = nullptr,
+          BinaryNode * right = nullptr)
+          : data{data},
+            left{left},
+            right{right} {}
+      BinaryNode(const T && data,
+          BinaryNode * left = nullptr,
+          BinaryNode * right = nullptr)
+          : data{std::move(data)},
+            left{left},
+            right{right} {}
+    };
+   
+    // member data 
+    BinaryNode * root;
+   
+    // private functions
+    // insertions
+    void insert(const T & data, BinaryNode * & node) {
+      if (node == nullptr) {
+        node = new BinaryNode(data);
+      } else if (this->node.left == nullptr) {
+        insert(data, node->left);
+      } else if (this->node.right == nullptr) {
+        insert(data, node->right);
+      }
+    }
+    void insert(T && data, BinaryNode * &node) {
+      if (node == nullptr) {
+        node = new BinaryNode(std::move(data));
+      } else if (this->node.left == nullptr) {
+        insert(data, node->left);
+      } else if (this->node.right == nullptr) {
+        insert(data, node->right);
+      }
+    }
+    // deletion
+    void remove(const T & data, BinaryNode * node) {
+      if (node->left == nullptr && node->right == nullptr) {
+        
+      }
+    }
     void printInfixExpression(BinaryNode * n);
     void makeEmpty(BinaryNode * n);
     BinaryNode * clone(BinaryNode) const;
     void printPostfixExpression(BinaryNode * n);
     size_t size(BinaryNode * t);
     size_t leaf_nodes(BinaryNode * t);
-    BinaryNode * root;
-
 };
 
 #include "bet.hpp"
